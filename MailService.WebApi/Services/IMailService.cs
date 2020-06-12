997b39a216ffa95372b731250eaf86214e906d06
Mukesh Murugan <iammukeshm@gmail.com>
2020-06-12 21:08:11 +0530

    Add project files.

---
+++ MailService.WebApi/Services/IMailService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailService.WebApi.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string body);
+
+
+    }
+}
---
+++ MailService.WebApi/Settings/MailSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MailService.WebApi.Settings
+{
+    public class MailSettings
+    {
+        public string Mail { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
